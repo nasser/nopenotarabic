0c981b52599953ea9c5a7baff07364b16b432b46
Ramsey Nasser <ram@nas.sr>
2020-11-30 19:06:27 -0500

    Remove unused imports

--- ssg.js
+++ ssg.js
@@ -1,14 +1,7 @@
 import fs from "fs"
-import path from "path"
 import YAML from "yaml"
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import hashString from 'hash-string'
-import http from 'http'
-import https from 'https'
-import mustache from 'mustache'
-import marked from 'marked'
-import moment from 'moment'
 import { downloadImages } from './download.js'
 import { build } from './render.js'
 
